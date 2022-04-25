4dcabe1ebb859180880546e301d94b774171c1e2
jbellon <josh@joshbellon.com>
2022-04-25 14:32:50 -0700

    Initial Commit. Express is set up and a flights schema exists

---
+++ models/flight.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ["American", "Southwest", "United"]
+    }
+    airport: {
+        type: String,
+        enum: ["AUS", "DFW", "DEN", "LAX", "SAN"]
+    }
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    }
+    departs: Date
+})
